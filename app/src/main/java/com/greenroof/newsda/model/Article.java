4e932a1087149098b4a91c80ebebe5ce4e7cca98
sjw <jwseo2698@1thefull.com>
2018-04-17 18:51:26 +0900

    article model 정의

--- app/src/main/java/com/greenroof/newsda/model/Article.java
+++ app/src/main/java/com/greenroof/newsda/model/Article.java
@@ -1,11 +1,129 @@
 package com.greenroof.newsda.model;
 
+import java.net.URL;
+
 /**
  * Created by sjw on 2018. 4. 17..
  */
 
 public class Article {
-    int id;
+    int id; //id
+    String category; //카테고리
+    String title; //제목
+    String content; //내용
+    String reporter; //작성자
+    String date; //날짜
+    int hits; //조회수
+    String press; //대분류(미디어/방송/신문)
+    String company; //소분류(언론사명)
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getReporter() {
+        return reporter;
+    }
+
+    public void setReporter(String reporter) {
+        this.reporter = reporter;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public void setHits(int hits) {
+        this.hits = hits;
+    }
+
+    public String getPress() {
+        return press;
+    }
+
+    public void setPress(String press) {
+        this.press = press;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getComment() {
+        return comment;
+    }
+
+    public void setComment(int comment) {
+        this.comment = comment;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getSearchWord() {
+        return searchWord;
+    }
+
+    public void setSearchWord(String searchWord) {
+        this.searchWord = searchWord;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    int comment; //댓글 수
+    int image; //이미지
+    String searchWord; //인기검색어
+    URL url;
 
 }
